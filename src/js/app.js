79510659867c649efebd34b7b33015d78fd14f36
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2016-03-28 07:56:16 -0400

    Added basic tab switch handler

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,21 @@ $(function () {
 
     // Set up the UI elements
     $('#app-state').buttonset();
-    $('#code-view').tabs({heightStyle: 'fill'});
+    $('#code-view').tabs({
+        heightStyle: 'fill',
+        activate: function (evt, ui) {
+            if (ui.newPanel.attr('id') === 'code-view-blocks') {
+                console.log("blocks");
+            }
+            else if (ui.newPanel.attr('id') === 'code-view-editor') {
+                console.log("editor");
+            }
+        }
+    });
     
     $('#sim-view').addClass('hidden');
     
-    // Set up toggles
+    // Set up behaviors
     $('#select-code-mode').click(function () {
         $('#code-view').removeClass('hidden');
         $('#sim-view').addClass('hidden');
@@ -19,6 +29,7 @@ $(function () {
         $('#sim-view').removeClass('hidden');
     });
 
+    // Set up Editor and Blockly
     var editor = ace.edit('code-view-editor');
     editor.setTheme('ace/theme/chrome');
     editor.getSession().setMode("ace/mode/javascript");
